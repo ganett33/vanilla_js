565fdb2f6651b976aab73eccb96bdc75650055ca
ganett33 <65261313+ganett33@users.noreply.github.com>
2021-04-21 15:59:19 +1200

    #2.7 DOM - If else - Function practice Two

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
-const title = document.getElementById("title");
-console.log(title)
+// const title = document.querySelector("#title");
 
-title.innerHTML ="Hi! from JS"
+// function handleClick() {
+//     title.style.color ="black";
+// }
+
+// title.addEventListener("click",handleClick);
+
+// const age = prompt ("How old are you");
+
+// if(age > 18 && age <=21) {
+//     console.log("You can drink but you should not");
+// }else if(age > 21) {
+//     console.log("GO AHEAD")
+// }else {
+//     console.log("Sorry mate!");
+// }
+
+// function handOffline() {
+//     console.log("Bye bye");
+// }
+
+// function handOnline() {
+//     console.log("Welcome back");
+// }
+
+// window.addEventListener("offline", handOffline)
+// window.addEventListener("online",handOnline)
+
+
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+    title.classList.toggle(CLICKED_CLASS)
+}
+
+function init() {
+    title.addEventListener("click",handleClick)
+}
+
+init();
+
